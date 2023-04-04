981aaa11b4958f1ea52356180d52f42431be2fcf
mettashalini89@gmail.com <CatMatDog@123>
2023-04-03 20:18:43 -0600

    jenkins practice

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,18 +1,29 @@
 def call() {
     pipeline {
         agent any
-            stages{
-                stage('Init'){
-                    steps{
-                        sh 'terraform init -backend-config=env-dev/state.tfvars'
-                    }
-                }
 
-                stage('Apply'){
-                    steps{
-                        sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
-                    }
+        parameters {
+            string(name: 'ENV', defaultValue: '', description: 'Which environment')
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+
+
+        stages{
+            stage('Init'){
+                steps{
+                    sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
+                }
+            }
+
+            stage('Apply'){
+                steps{
+                    sh 'echo'
+                    //sh 'terraform apply -auto-approve -var-file=env-dev/main.tfvars'
                 }
             }
         }
     }
+}
