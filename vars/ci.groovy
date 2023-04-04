cd80b8ae6c08f5bc3e8bc9bb044941191d49afdf
mettashalini89@gmail.com <CatMatDog@123>
2023-04-04 16:50:03 -0600

    jenkins practice

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -5,10 +5,10 @@ def call() {
             stage('Compile/Build') {
                 steps {
                     script {
-                        if (env_lang == "nodejs"){
+                        if (env_lang == "nodejs") {
                             sh "npm install"
                         }
-                        if (env_lang == "maven"){
+                        if (env_lang == "maven") {
                             sh "mvn package"
                         }
                     }
