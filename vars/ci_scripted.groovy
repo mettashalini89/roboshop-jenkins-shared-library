53a0ea6e84e8f706f6d5917b16458984291b928f
mettashalini89@gmail.com <CatMatDog@123>
2023-04-12 14:24:21 -0600

    jenkins practice

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -9,7 +9,7 @@ def call() {
                 sh 'ls -l'
                 cleanWs()
                 sh 'ls -l'
-                git branch: 'main', url: 'https://github.com/mettashalini89/cart'
+                git branch: 'main', url: 'https://github.com/mettashalini89/cart.git'
                 sh 'ls -l'
             }
 
