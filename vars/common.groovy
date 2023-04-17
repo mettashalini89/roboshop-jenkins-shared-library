ba747abdf8e391475545e9e0affa2795ea67c239
mettashalini89@gmail.com <CatMatDog@123>
2023-04-17 11:50:23 -0600

    jenkins practice

--- vars/common.groovy
+++ vars/common.groovy
@@ -25,16 +25,16 @@ def qualitycheck() {
 
 def prepareArtifacts() {
     sh 'echo ${TAG_NAME} >VERSION'
-/*    if (app_lang == "nodejs" || app_lang == "angular"){
+    if (app_lang == "nodejs" || app_lang == "angular"){
         sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
-    }*/
+    }
 
 }
 
 def artifactUpload() {
     sh 'echo ${TAG_NAME} >VERSION'
-/*    if (app_lang == "nodejs" || app_lang == "angular"){
+    if (app_lang == "nodejs" || app_lang == "angular"){
         sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.14.183:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-    }*/
+    }
 }
 
