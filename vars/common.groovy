196f0b0a5d3908ed29b630794f144965400a1b4e
mettashalini89@gmail.com <CatMatDog@123>
2023-05-12 16:59:13 -0600

    kubernets practice

--- vars/common.groovy
+++ vars/common.groovy
@@ -24,12 +24,14 @@ def qualitycheck() {
 }
 
 def prepareArtifacts() {
-    sh 'echo ${TAG_NAME} >VERSION'
-    if (app_lang == "maven"){
-        sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
-    }else {
-        sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
-    }
+    //sh 'echo ${TAG_NAME} >VERSION'
+    //if (app_lang == "maven"){
+    //    sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar schema VERSION'
+    //}else {
+    //    sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
+    //}
+
+    docker build -t 780395309002.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .
 
 }
 
